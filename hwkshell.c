d239e51b52890fc5c638fa41869e88246cb5e252
Simon Cole <simonccole1@gmail.com>
2022-03-19 13:05:12 -0400

    iterative commit before using strtok

--- hwkshell.c
+++ hwkshell.c
@@ -7,26 +7,34 @@
 char * myargv[100];
 
 void processInput(char input[], char * myargv[], char * myenv[]);
+char * getEnvPath(char * myEnv[]);
 
 
 int main(int argc, char * argv[], char * env[]){
-	char input[100];
-	int i = 0;
-	while(1){
-		printf(">> ");
-		fgets(input, 100, stdin); 
-		input[strlen(input)-1] = '\0';
-		if(strlen(input) == 0)
-			continue;
-		if(strcmp(input, "exit") == 0)
-			break;
-		processInput(input, argv, env);
-	}
-return 0;
+	// char input[100];
+	// int i = 0;
+	// while(1){
+	// 	printf(">> ");
+	// 	fgets(input, 100, stdin); 
+	// 	input[strlen(input)-1] = '\0';
+	// 	if(strlen(input) == 0)
+	// 		continue;
+	// 	if(strcmp(input, "exit") == 0)
+	// 		break;
+	// 	processInput(input, argv, env);
+	// }
+	char * path;
+	path = getEnvPath(env);
+	printf("%s", path);
+	
+	
+	return 0;
 }
 
 void processInput(char input[], char * myargv[], char * myenv[]){
+	//get input like normal and print out the env recieved
 	int i = 0, status;
+	char path[1000];
 	char * str = strtok(input, " ");
 	while(str){
 		myargv[i] = str;
@@ -41,14 +49,29 @@ void processInput(char input[], char * myargv[], char * myenv[]){
 	if(pid == 0)
 	{
 		char cmd[32];
-		char * path = "/bin/"; //Note: This requires work you need toget the path from the PATH environemt variable.
+		char * path = myenv[8]; //Note: This requires work you need toget the path from the PATH environemt variable.
 		strcpy(cmd, path);
 		strcat(cmd, myargv[0]);
 		int r = execve(cmd, myargv, myenv);
-		printf("Cannot execturecommand. %04X\n", r);
+		printf("Cannot exectue command. %04X\n", r);
 		return;
 	}
 
 	wait(&status);
+}
+
+char * getEnvPath(char * myEnv[]){
+	//env[13]
+	char path[1000]; 
+	char fullPath[1000];
+	char charStr[2] = "\0";
+	int size = strlen(myEnv[13]);
+	strcpy(fullPath, myEnv[13]);
+	for(int i = 5; i < size; i++){
+		charStr[0] = fullPath[i];
+		strcat(path, charStr);
+	}
+	return path;
+	
 }
 
